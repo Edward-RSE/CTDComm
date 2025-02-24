ca3482e23b8b896020c523a984669528c9d03625
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2025-02-24 14:16:28 +0000

    adding example scripts

---
+++ scripts/iridisX_scripts/train_dpp_5v1_tarmac_cpu.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --partition=amd
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --time=01:00:00
+
+source .venv/bin/activate
+export PYTHONUNBUFFERED=1
+
+if [ $# -eq 1 ]; then
+  seed=$1
+else
+  seed=1
+fi
+
+printf -v date '%(%Y-%m-%d_%H:%M:%S)T' -1
+
+python -u run_baselines.py \
+  --env_name dec_predator_prey \
+  --nagents 5 \
+  --dim 10 \
+  --max_steps 40 \
+  --vision 1 \
+  --nprocesses $SLURM_NTASKS \
+  --num_epochs 100 \
+  --epoch_size 10 \
+  --hid_size 128 \
+  --value_hid_size 128 \
+  --value_coeff 0.01 \
+  --detach_gap 10 \
+  --ic3net \
+  --tarcomm \
+  --recurrent \
+  --comm_passes 2 \
+  --save \
+  --save_every 25 \
+  --seed $seed \
+  --env_seed $seed \
+  --alpha 0.99 \
+  --gamma 0.99 \
+  --entr 0.01 \
+  --lrate 0.0007
+
---
+++ scripts/iridisX_scripts/train_dpp_5v1_tarmac_l4.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#SBATCH --partition=swarm_l4
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+#SBATCH --time=01:00:00
+
+source .venv/bin/activate
+export CUDA_VISIBLE_DEVICES=0
+export PYTHONUNBUFFERED=1
+
+if [ $# -eq 1 ]; then
+  seed=$1
+else
+  seed=1
+fi
+
+printf -v date '%(%Y-%m-%d_%H:%M:%S)T' -1
+
+python -u run_baselines.py \
+  --cuda \
+  --env_name dec_predator_prey \
+  --nagents 5 \
+  --dim 10 \
+  --max_steps 40 \
+  --vision 1 \
+  --nprocesses 1 \
+  --num_epochs 100 \
+  --epoch_size 10 \
+  --hid_size 128 \
+  --value_hid_size 128 \
+  --value_coeff 0.01 \
+  --detach_gap 10 \
+  --ic3net \
+  --tarcomm \
+  --recurrent \
+  --comm_passes 2 \
+  --save \
+  --save_every 25 \
+  --seed $seed \
+  --env_seed $seed \
+  --alpha 0.99 \
+  --gamma 0.99 \
+  --entr 0.01 \
+  --lrate 0.0007
+
+# Parameters from TarMAC paper.
+  # --alpha 0.99 \
+  # --gamma 0.99 \
+  # --entr 0.01 \
+  # --lrate 0.0007 \
