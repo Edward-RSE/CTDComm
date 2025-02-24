7d499d6a5e897860aca8936fa4b51f5ec7c1f78d
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2025-02-24 11:36:11 +0000

    go back to more separated structure and improve robustness of installation

--- python_envs/install-venv.sh
+++ python_envs/install-venv.sh
@@ -2,26 +2,29 @@
 # Setup a Python virtual environment and install required dependencies.
 # Requires Python > 3.12
 
-REQUIRED_PYTHON_VERSION="3.12"
-INSTALLED_PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+MINIMUM_REQUIRED_PYTHON_VERSION="3.12"
 
 install_venv() {
-    python -m venv .venv
+    python3 -m venv .venv
 }
 
 install_packages() {
     pip install -r python_envs/requirements.txt
-    pip install .
-    cd src/envs/decentralized-envs && pip install .
+    pip install -e .
+    cd learning_envs/decentralised-envs && pip install .
     cd ../ic3net-envs && pip install .
 }
 
-if ! command -v python3 &> /dev/null; then
-    echo "Error: Python is not installed"
-    exit 1
-fi
-if [ "$INSTALLED_PYTHON_VERSION" != "$REQUIRED_PYTHON_VERSION" ]; then
-    echo "Error: Python $REQUIRED_PYTHON_VERSION is required, but $INSTALLED_PYTHON_VERSION is installed."
+if command -v python3.12 &> /dev/null; then
+    INSTALLED_PYTHON_VERSION=312
+elif command -v python3 &> /dev/null; then
+    INSTALLED_PYTHON_VERSION=$(python3 -c 'import sys; print(sys.version_info[0]*100 + sys.version_info[1])')
+    if [[ $INSTALLED_PYTHON_VERSION -lt $MINIMUM_REQUIRED_PYTHON_VERSION ]]; then
+        echo "Error: At least Python 3.12 is required, but found Python $INSTALLED_PYTHON_VERSION."
+        exit 1
+    fi
+else
+    echo "Error: Python3 is not installed."
     exit 1
 fi
 
